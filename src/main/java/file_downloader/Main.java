840be41c232b04374f73abaa814d254706b1c5dd
Omar-Njie <omzinho8051113@gmail.com>
2022-03-01 10:21:02 -0800

    changed few things

--- src/main/java/file_downloader/Main.java
+++ src/main/java/file_downloader/Main.java
@@ -6,9 +6,6 @@ import javax.swing.*;
 
 public class Main {
 
-    public static String custom_directory;
-    public static boolean custom_directory_flag;
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             dark_mode();
