6b385fbaa09efb7ac474a01e75624cdbae6865b4
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-09-13 06:26:44 -0700

    updated main

--- src/main/java/file_downloader/Main.java
+++ src/main/java/file_downloader/Main.java
@@ -1,16 +1,15 @@
 package file_downloader;
 
-import javax.swing.*;
+import themes.DefaultTheme;
 
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            // Dark dark_mode = new Dark();
-            //dark_mode.run();
+            DefaultTheme.default_theme();
             new Settings();
-            new Ui();
         });
     }
 }
