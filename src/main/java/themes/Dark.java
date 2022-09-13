779ad0c9c9c971aaa55cefbcbf2cf3ea5a8a415a
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-09-13 05:34:21 -0700

    added dark theme support

--- src/main/java/themes/Dark.java
+++ src/main/java/themes/Dark.java
@@ -4,30 +4,13 @@ import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialDeepOc
 
 import javax.swing.*;
 
-public class Dark implements Runnable {
+public class Dark {
 
-    private final int thread_number;
-
-    public Dark(int thread_number) {
-        this.thread_number = thread_number;
-    }
-
-    @Override
-    public void run() {
-        System.out.println("Dark mode is on!");
-        for (int i = 0; i <= 1; i++) {
-            System.out.println("Dark Theme [" + i + "] running at " + thread_number);
-            try {
-                UIManager.setLookAndFeel(new FlatMaterialDeepOceanContrastIJTheme());
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    public static void dark_mode() {
+        try {
+            UIManager.setLookAndFeel(new FlatMaterialDeepOceanContrastIJTheme());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-
 }
