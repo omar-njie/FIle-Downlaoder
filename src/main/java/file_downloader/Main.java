8fafd96d175763b63a7b33d2abe29e8f027916bd
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-10-21 17:03:58 -0700

    added menu bar for macOS & accent color

--- src/main/java/file_downloader/Main.java
+++ src/main/java/file_downloader/Main.java
@@ -7,6 +7,13 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+        String os = System.getProperty("os.name").toLowerCase();
+        OsIdentifier osIdentifier = new OsIdentifier(os);
+        if (osIdentifier.is_mac()) {
+            System.setProperty("apple.awt.application.appearance", "system");
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+        }
+
         SwingUtilities.invokeLater(() -> {
             DefaultTheme.default_theme();
             new Settings();
