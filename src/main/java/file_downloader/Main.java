85413a788e85e8b62d3270f9392f81151a06c10d
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-03-20 03:51:00 -0700

    refracted

--- src/main/java/file_downloader/Main.java
+++ src/main/java/file_downloader/Main.java
@@ -9,14 +9,16 @@ public class Main {
     public static void main(String[] args) {
         String os = System.getProperty("os.name").toLowerCase();
         OsIdentifier osIdentifier = new OsIdentifier(os);
-        if (osIdentifier.is_mac()) {
-            System.setProperty("apple.awt.application.appearance", "system");
-            System.setProperty("apple.laf.useScreenMenuBar", "true");
-        }
 
         SwingUtilities.invokeLater(() -> {
+            if (osIdentifier.is_mac()) {
+                System.setProperty("apple.awt.application.appearance", "system");
+                System.setProperty("apple.laf.useScreenMenuBar", "true");
+            }
+            // Apply the default theme
             DefaultTheme.default_theme();
             new Settings();
         });
     }
+
 }
